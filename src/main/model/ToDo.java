56b09679d9997c5d83860f4d7dc47b4e0fd942ea
UBC Student <github.students.cs.ubc.ca>
2019-10-04 17:24:43 -0700

    Fixed load and save test to use new files

---
+++ src/main/model/ToDo.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ToDo {
+    private String todoName;
+
+    public ToDo(String s) {
+        todoName = s;
+    }
+
+    public String getToDoName() {
+        return todoName;
+    }
+}
