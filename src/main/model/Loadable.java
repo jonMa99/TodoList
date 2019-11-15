bf8c32b14f9a42df10f4fcbf9cb56eb41418d948
UBC Student <github.students.cs.ubc.ca>
2019-11-15 15:51:24 -0800

    Fixed specifications

--- src/main/model/Loadable.java
+++ src/main/model/Loadable.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public interface Loadable {
 
+    // MODIFIES: removeToDoList, toDoList, urgentToDoList
     // EFFECT: loads removeToDoListoutput.txt and toDoListoutput.txt files
     public void load(String toDo, String urgenttoDo, String removeList) throws IOException;
 }
---
+++ src/main/ui/MainMenu.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainMenu {
+    private JButton addToDoButton;
+    private JButton addUrgentToDoButton;
+    private JButton deleteToDoButton;
+    private JButton showToDosAndUrgentToDosButton;
+    private JButton searchToDosByLocationButton;
+    private JButton saveAndQuitButton;
+}
