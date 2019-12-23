461d318e247decf0948a5ec1c764e5c190f7b236
i8t2b <mager123@students.cs.ubc.ca>
2019-12-22 16:23:56 -0800

    Test

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //new UserPrompts();
-        //new MainMenu("ToDoList");
         new MainGUI();
     }
 }
